47109672f4005678a0b96b00aaf8bee7b1bbb800
le-quoctrung <20127369@student.hcmus.edu.vn>
2021-04-03 08:58:48 +0700

    Setup
    setup console window when run program

---
+++ main/Course-Registration-System/Console.h
@@ -0,0 +1,26 @@
+#pragma once
+#pragma once
+#include<iostream>
+#include<fstream>	
+#include<Windows.h>
+#include<string>
+#include<conio.h>
+#include<stdio.h>
+
+
+
+using namespace std;
+
+
+void Gotoxy(int x, int y);
+void ResizeConsole(int width, int height);// default 300-80
+void SetTextColor(int backgound_color/*default white*/, int text_color);
+void HienTroChuot();
+void AnTroChuot();
+void FixConsoleWindow();
+void DisableResizeWindow();
+void DisableCtrButton(bool Close, bool Min, bool Max);	// 1 to active
+void ShowScrollbar(bool Show);
+void DisableSelection();
+
+void Start(); // call all function need to run the program
