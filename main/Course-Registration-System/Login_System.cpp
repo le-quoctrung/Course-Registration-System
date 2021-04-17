56dc855ee7829278388edc48d630ba7cad09584e
Shenronis <kakarotto16122001@gmail.com>
2021-04-17 22:37:54 +0700

    Update
    Remove:
    + checkLogin(): moved to file.h

--- main/Course-Registration-System/Login_System.cpp
+++ main/Course-Registration-System/Login_System.cpp
@@ -1,71 +1,14 @@
 #include "Login_System.h"
 
-int checkLogin(std::string name, std::string pass)
-{
-	if (!checkFile(name)) return 404;
-
-	std::ifstream file("db\\students.txt", std::ios::in);
-	if (file.is_open())
-	{
-		std::string check;
-		std::vector<std::string> line;
-
-		while (!file.eof())
-		{
-			std::getline(file, check);
-
-			line = split(check, ",");
-
-			if (name == line[0])
-			{
-				//check password
-				if (pass != line[1])
-				{
-					return -1;
-				}
-				else return 0;
-			}
-		}
-		file.close();
-	}
-
-	file.open("db\\staffs.txt", std::ios::in);
-	if (file.is_open())
-	{
-		std::string check;
-		std::vector<std::string> line;
-
-		while (!file.eof())
-		{
-			std::getline(file, check);
-
-			line = split(check, ",");
-
-			if (name == line[0])
-			{
-				//check password
-				if (pass != line[1])
-				{
-					return -1;
-				}
-				else return 1;
-			}
-		}
-		file.close();
-	}
-
-	return 404;
-}
-
 void login()
 {
 	string name="", pass = "";
 	char chpass, chname;
-	
+
 	Gotoxy(80, 13);
 	chname = _getch();
 	while (chname != 13) {//character 13 is enter
-		if (name.length() > 7 && chname != '\b') chname = '\0';
+		if (name.length() > 50 && chname != '\b') chname = '\0';
 		else if (chname == '\b' && name.length() >= 1)//if user typed backspace 
 		{
 			cout << "\b \b";//rub the character behind the cursor.
@@ -98,13 +41,33 @@ void login()
 		chpass = _getch();
 	}
 	system("cls");
-	switch (checkLogin(name,pass))
+	switch (CheckLogin(name,pass))
 	{
-	case 404: home(); Gotoxy(80, 17); cout << "Login failed. Account not found"; break;
-	case -1: home(); Gotoxy(80, 17); cout << "Login failed. Wrong password"; break;
-	case 1: break;
-	case -2: break;
-	case 0: break;
+	case 404: 
+	{
+		home();
+		Gotoxy(80, 17); 
+		cout << "Login failed. Account not found!"; 
 		break;
+	}
+	case -1:
+	{
+		home();
+		Gotoxy(80, 17);
+		cout << "Login failed. Username and Password missmatch!"; 
+		break;
+	}
+	case 1: 
+	{
+		Gotoxy(60, 17);
+		std::cout << "WELCOME BACK HCMUS STAFF! HAVE A NICE WEEK!";
+		break;
+	}
+	case 0: 
+	{
+		Gotoxy(60, 17);
+		std::cout << "WELCOME BACK TO HELL STUDENT!"; 
+		break;
+	}
 	}
 }
--- main/Course-Registration-System/Login_System.h
+++ main/Course-Registration-System/Login_System.h
@@ -5,27 +5,14 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include<conio.h>
+#include <conio.h>
 
 #include "school.h"
-#include"Console.h"
-#include"display.h"
+#include "Console.h"
+#include "display.h"
 #include "file.h"
 
-struct User
-{
-	std::string _name;
-	std::string _pass;
-	bool type; 	//0 - student
-				//1 - staff
-};
-
 /*	FUNCTIONS	*/
-int checkLogin(std::string name, std::string pass);					//	checkLogin returns
-																	//	-1	- password not match
-																	//	0	- student
-																	//	1	- staff
-																	//	404 - file not found
-
 void login();														//call first to run all login function
+
 #endif // !_LOGIN_SYSTEM_H_
