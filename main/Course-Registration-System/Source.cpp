933d2b88cfc05909d18b2c25dd6695d137d99882
Shenronis <hoangthien161201@gmail.com>
2021-04-19 10:19:07 +0700

    main

--- main/Course-Registration-System/Source.cpp
+++ main/Course-Registration-System/Source.cpp
@@ -11,7 +11,32 @@
 
 int main()
 {
+	//ListClass* classes = new ListClass;
+	//createEmptyList(classes);
+	////add node class
+	//addClass(classes, 1, "CLC");
 
+	////read from list to node class
+	//ReadListToClass("allStudent.csv", getNode(classes->head,1));
+	//OutputListStudents(getNode(classes->head, 1));
+	//system("pause");
+	//CreateStudentAccounts(getNode(classes->head, 1));
+
+	/*
+	
+	to do list:
+	session
+	update course function
+
+	menu for staffs
+	menu for students
+	
+	export csv file for grading
+	import csv grading file
+
+
+
+	*/
 
 	return 0;
 
