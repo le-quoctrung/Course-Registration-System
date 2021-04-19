d66372725ef73d8dc8ede3c2e01b23b1645d85c1
Shenronis <hoangthien161201@gmail.com>
2021-04-19 10:18:09 +0700

    update
    add checkValid()

--- main/Course-Registration-System/time.cpp
+++ main/Course-Registration-System/time.cpp
@@ -61,3 +61,30 @@ void copyDate(date& a, date b)
 	a.month = b.month;
 	a.day = b.day;
 }
+
+bool checkValid(date D)
+{
+	int d = D.day;
+	int m = D.month;
+	int y = D.year;
+
+	if (!(1 <= m && m <= 12))
+		return false;
+	if (!(1 <= d && d <= 31))
+		return false;
+	if ((d == 31) && (m == 2 || m == 4 || m == 6 || m == 9 || m == 11))
+		return false;
+	if ((d == 30) && (m == 2))
+		return false;
+	if ((m == 2) && (d == 29) && (y % 4 != 0))
+		return false;
+	if ((m == 2) && (d == 29) && (y % 400 == 0))
+		return true;
+	if ((m == 2) && (d == 29) && (y % 100 == 0))
+		return false;
+	if ((m == 2) && (d == 29) && (y % 4 == 0))
+		return true;
+
+	return true;
+	
+}
